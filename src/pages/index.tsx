ecc59794533f64f871e1de958fefba9377e7d44d
Giovani Machado Correa <giovanimcorrea18@gmail.com>
2021-09-16 10:21:55 -0300

    creating structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/home.module.scss'
+
+
+export default function Home() {
+  return (
+    <div className={styles.title}>
+      <h1>Oieeeeeeeeeeeee</h1>
+      <h2>desgraça para de tirar o parenteses</h2>
+    </div>
+  );
+}
