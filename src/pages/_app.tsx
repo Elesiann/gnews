92c38e42bbcd0b71215d5bb813ef9ecb801e1ed6
Giovani Machado Correa <giovanimcorrea18@gmail.com>
2021-09-16 16:38:22 -0300

    header styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { AppProps } from "next/app";
+import React from "react";
+import { Header } from "../components/Header";
 
-import "../styles/global.scss"
+import "../styles/global.scss";
 
 // app é o carinha que se repete em todas as páginas
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
