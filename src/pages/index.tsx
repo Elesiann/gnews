01fb6f256a090ce1d427750fad1e7c762a213d74
Giovani <giovanimcorrea18@gmail.com>
2021-09-23 19:54:20 -0300

    SSG (Static Site Generation) with revalidate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import styles from './home.module.scss'
 import { stripe } from "../services/stripe";
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1JceBUFgPxF17pln2vg9l8fF')
 
@@ -55,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24 // função que recria o html da página em tantos minutos, neste caso, 24 horas
   }
 }
