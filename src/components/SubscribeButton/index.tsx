400a97d80fe81b242d8be6da13b5b78ff273c304
Giovani Machado Correa <giovanimcorrea18@gmail.com>
2021-09-28 16:44:57 -0300

    redirecting to stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,21 +1,32 @@
 import { signIn, useSession } from "next-auth/client";
 import styles from "./styles.module.scss";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-    
-
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
 
-  function handleSubscribe() {
-      if(!session) {
-          signIn('github')
-          return
-      }
-      // criação da checkout session
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      console.log(err)
+      alert(err.message);
+    }
   }
 
   return (
