34a6835291b6161e8333022092e0193d78efd75f
Giovani <giovanimcorrea18@gmail.com>
2021-09-29 22:05:07 -0300

    stripe webhooks

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -24,7 +24,6 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
       await stripe.redirectToCheckout({ sessionId });
     } catch (err) {
-      console.log(err)
       alert(err.message);
     }
   }
